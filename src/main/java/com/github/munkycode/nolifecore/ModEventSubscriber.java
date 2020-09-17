d705efe73bf4a9cb47b1ddf2c943dda29246e398
Stephen <stevoschool@gmail.com>
2020-09-17 15:11:54 -0400

    added block

--- src/main/java/com/github/munkycode/nolifecore/ModEventSubscriber.java
+++ src/main/java/com/github/munkycode/nolifecore/ModEventSubscriber.java
@@ -2,6 +2,9 @@ package com.github.munkycode.nolifecore;
 
 
 import com.github.munkycode.nolifecore.init.ModItemGroups;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
@@ -16,6 +19,11 @@ public class ModEventSubscriber {
     public static void onRegisterItems(RegistryEvent.Register<Item> event){
         event.getRegistry().registerAll(setup(new Item(new Item.Properties().group(ModItemGroups.NOLIFECORE)), "example_ingot"));
 
+    }
+
+    @SubscribeEvent
+    public static void onRegisterBlocks(RegistryEvent.Register<Block> event){
+        event.getRegistry().registerAll(setup(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)), "example_ore"));
     }
 
     public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
